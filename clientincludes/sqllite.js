cfb84969ca5775c11896457cab3eecee05f313f7
Michail Michailidis <mmichaelid@gmail.com>
2013-06-23 23:57:29 -0400

    database manipulation also works with dispatcher

--- clientincludes/sqllite.js
+++ clientincludes/sqllite.js
@@ -1,75 +1,115 @@
-function createDatabaseIfNotExists(dbname,dbsize){
-	var db = openDatabase(dbname, '1.0', 'Test DB2', dbsize);
-	db.createTilesTableIfNotExists = function() {
-		this.transaction(function (tx) {
-			tx.executeSql('CREATE TABLE IF NOT EXISTS Tiles (id INTEGER PRIMARY KEY,x,y,base64DataUrl,zoomLevel,stratPercent)');
-			msg = '<p>Log message: created table.</p>';
-			document.querySelector('#status').innerHTML +=  msg;
-		});
-	}
-
-	db.dropTilesTableIfExists = function() {
-		this.transaction(function (tx) {
-			tx.executeSql('DROP TABLE IF EXISTS Tiles',[]); 
-		  	msg = '<p>Log message: dropped table.</p>';
-		  	document.querySelector('#status').innerHTML +=  msg; 
-		});
-	}
-
-  	db.insertTileData = function (tilePosX, tilePosY, tileZoomLevel,base64DataUrl){
-  		this.transaction(function (tx) {
-			tx.executeSql('INSERT INTO Tiles (zoomLevel, x,y,base64DataUrl) VALUES ('+tileZoomLevel+','+tilePosX+','+tilePosY+',"'+base64DataUrl+'")');
+function tileInsert(config,dispatcher){
+	var db = config["db"];
+	var tile = config["tile"];
+	var tileZoomLevel = tile.zoomLevel;
+	var tilePosX = tile.posX;
+	var tilePosY = tile.posY;
+	var base64DataUrl = tile.base64DataUrl;
+	db.transaction(function (tx) {
+	tx.executeSql('INSERT INTO Tiles (zoomLevel, x,y,base64DataUrl) VALUES ('+tileZoomLevel+','+tilePosX+','+tilePosY+',"'+base64DataUrl+'")',[],
+		function(tx,results){
+			console.log("WWW"+results.insertId);
+			tile.id = results.insertId;
 			msg = '<p>Log message: inserted row.</p>';
 			document.querySelector('#status').innerHTML +=  msg;
+			dispatcher.check();
 		});
-	}
+	});
+	
+}
 
-
-	db.updateTilePixelDataWithId = function(id,base64DataUrl){
-		this.transaction(function (tx) {
-			tx.executeSql('UPDATE Tiles SET base64DataUrl = "'+base64DataUrl+'" WHERE id='+id);
-			msg = '<p>Log message: updated row with id='+id+'.</p>';
+function tileFetchById(config,dispatcher){
+	var id = config["id"];
+	var db = config["db"];
+	db.transaction(function (tx) {
+		tx.executeSql('SELECT * FROM Tiles WHERE id='+id, [], function (tx, results) {
+			var len = results.rows.length, i;
+			msg = "<p>Found tile with id: " + id + "</p>";
 			document.querySelector('#status').innerHTML +=  msg;
-		});
+			//if (len>=1){
+				row = results.rows.item(0);
+				tile = extractTile(row);
+				//render(tile);
+				console.log("here");
+				dispatcher.check();
+				dispatcher.addNew({func: render,config: {tile: tile}});
+			//}
+		}, null);
+	});
+}
+
+function tileFetchByPosition(config,dispatcher){
+	var posX = config("posX");
+	var posY = config("posY");
+	var db = config("db");
+	db.transaction(function (tx) {
+		tx.executeSql('SELECT * FROM Tiles WHERE x ='+posX+' AND y='+posY, [], function (tx, results) {
+			var len = results.rows.length, i;
+			msg = "<p>Found tile with position: " + posX + ","+ posY + "</p>";
+			document.querySelector('#status').innerHTML +=  msg;
+			if (len==1){
+				row = results.rows.item(0);
+				var tile = extractTile(row);
+			}
+		}, null);
+	});
+}
+
+
+function createDatabaseIfNotExists(dbname,dbsize){
+	return openDatabase(dbname, '1.0', 'Test DB2', dbsize);
+}
+
+function createTilesTableIfNotExists(db) {
+	db.transaction(function (tx) {
+		tx.executeSql('CREATE TABLE IF NOT EXISTS Tiles (id INTEGER PRIMARY KEY,x,y,base64DataUrl,zoomLevel,stratPercent)');
+		msg = '<p>Log message: created table.</p>';
+		document.querySelector('#status').innerHTML +=  msg;
+	});
+}
+
+
+function dropTilesTableIfExists(db) {
+	db.transaction(function (tx) {
+		tx.executeSql('DROP TABLE IF EXISTS Tiles',[]); 
+	  	msg = '<p>Log message: dropped table.</p>';
+	  	document.querySelector('#status').innerHTML +=  msg; 
+	});
+}
+
+  	
+
+function tilePixelDataUpdate(config,dispatcher){
+	var db = config["db"];
+	var tile = config["tile"];
+	var id;
+	var base64DataUrl;
+	if (tile){
+		id = tile.id;
+		console.log(id);
+		base64DataUrl = tile.base64DataUrl;
 	}
-
-	db.fetchTileWithId = function(id,extractTile,render,dispatcher){
-		this.transaction(function (tx) {
-			tx.executeSql('SELECT * FROM Tiles WHERE id='+id, [], function (tx, results) {
-				var len = results.rows.length, i;
-				msg = "<p>Found tile with id: " + id + "</p>";
-				document.querySelector('#status').innerHTML +=  msg;
-				//if (len>=1){
-					row = results.rows.item(0);
-					tile = extractTile(row);
-					//render(tile);
-					console.log("here");
-					dispatcher.check();
-					dispatcher.addNew({func: render,config: {tile: tile}});
-
-				//}
-			}, null);
-		});
+	else {
+		var id = config["id"];
+		var base64DataUrl = config["base64DataUrl"];
 	}
+	db.transaction(function (tx) {
+		tx.executeSql('UPDATE Tiles SET base64DataUrl = "'+base64DataUrl+'" WHERE id='+id);
+		msg = '<p>Log message: updated row with id='+id+'.</p>';
+		document.querySelector('#status').innerHTML +=  msg;
+	});
+}
 
-	db.fetchTileWithPosition = function(x,y,extractTile,renderFlag){
-		this.transaction(function (tx) {
-			tx.executeSql('SELECT * FROM Tiles WHERE x ='+x+' AND y='+y, [], function (tx, results) {
-				var len = results.rows.length, i;
-				msg = "<p>Found tile with position: " + x + ","+ y + "</p>";
-				document.querySelector('#status').innerHTML +=  msg;
-				if (len==1){
-					row = results.rows.item(0);
-					var tile = extractTile(row);
-					if (renderFlag){
-						tile.render();
-					}
-				}
-			}, null);
-		});
-	}
 
-	db.fetchAllTiles = function(extractTile,renderFlag){
+$(function() {
+
+});
+
+	
+
+	
+
+	/*db.fetchAllTiles = function(extractTile,renderFlag){
 		this.transaction(function (tx) {
 			tx.executeSql('SELECT * FROM Tiles', [], function (tx, results) {
 				var len = results.rows.length, i;
@@ -85,5 +125,4 @@ function createDatabaseIfNotExists(dbname,dbsize){
 			}, null);
 		});
 	}
-  return db;
-}
+  return db;*/
--- clientincludes/visual.js
+++ clientincludes/visual.js
@@ -1,192 +1,203 @@
-$(function() {
+function newEl(tag){
+		return document.createElement(tag);
+}
 
-	function newEl(tag){
-			return document.createElement(tag);
+function convertPixelsArrayToCanvas(pixelsArray, width, height){
+	
+	var mCanvas = newEl('canvas');
+	mCanvas.width = width;
+	mCanvas.height = height;
+	
+	var mContext = mCanvas.getContext('2d');
+	var mImgData = mContext.createImageData(width, height);
+	
+	var srcIndex=0, dstIndex=0, curPixelNum=0;
+	
+	for (curPixelNum=0; curPixelNum<width*height;  curPixelNum++)
+	{
+		mImgData.data[dstIndex] = pixelsArray[srcIndex][0];		// r
+		mImgData.data[dstIndex+1] = pixelsArray[srcIndex][1];	// g
+		mImgData.data[dstIndex+2] = pixelsArray[srcIndex][2];	// b
+		mImgData.data[dstIndex+3] = 255; // 255 = 0xFF - constant alpha, 100% opaque
+		srcIndex += 1;
+		dstIndex += 4;
+	}
+	mContext.putImageData(mImgData, 0, 0);
+
+	mCanvas.convertToBase64 = function(){
+		return (String)(this.toDataURL());
+	}
+	return mCanvas;
+}
+
+function convertBase64toCanvas(myImage,width,height){
+	var start = new Date().getMilliseconds();
+	var mCanvas = newEl("canvas");
+	mCanvas.width = width;
+	mCanvas.height = height;
+	mCanvas.setAttribute('style', "width:"+10*width+"px; margin-left: 2px; margin-bottom:2px; height:"+10*height+"px;"); // make it large enough to be visible
+	var mContext = mCanvas.getContext('2d');
+  	mContext.drawImage(myImage, 0, 0);
+    var end = new Date().getMilliseconds();
+	//console.log((end-start)+"ms");
+	return mCanvas;
+}
+
+function convertCanvasToPixelsArray(mCanvas){
+	var mContext = mCanvas.getContext('2d');
+	console.log(mCanvas.width);
+	var mImgData = mContext.getImageData(0,0,mCanvas.width,mCanvas.height);
+	
+	var srcIndex=0, dstIndex=0, curPixelNum=0;
+	var pixelsArray = new Array(mCanvas.width*mCanvas.height);
+	for (curPixelNum=0; curPixelNum<mCanvas.width*mCanvas.height;  curPixelNum++)
+	{
+		pixelsArray[dstIndex] = new Array(3);
+		pixelsArray[dstIndex][0] = mImgData.data[srcIndex];	// r
+		pixelsArray[dstIndex][1] = mImgData.data[srcIndex+1];	// g
+		pixelsArray[dstIndex][2] = mImgData.data[srcIndex+2];	// b
+		//mImgData.data[dstIndex+3] = 255; // 255 = 0xFF - constant alpha, 100% opaque
+		dstIndex += 1;
+		srcIndex += 4;
+	}
+	return pixelsArray;
+}
+
+function initialize_tile(config){
+	var x = config["x"];
+	var y = config["y"];
+	var width = config["width"];
+	var height = config["height"];
+	
+	var zoomLevel = config["zoomLevel"];
+	var pixelsArray;
+	var tile = new Object();
+	tile.width = width;
+	tile.height = height;
+	tile.posX = x;
+	tile.posY = y;
+	tile.zoomLevel = zoomLevel;
+	if (config["base64DataUrl"]){
+		tile.base64DataUrl = config["base64DataUrl"];
+	}
+	else {
+		var defaultColor = config["defaultColor"];
+		pixelsArray = initialize_pixelsArray({width: width,height: height,defaultColor: defaultColor});
+		tile.base64DataUrl = convertPixelsArrayToCanvas(pixelsArray,tile.width,tile.height).convertToBase64();
+	}
+	if (config["id"]){
+		id = config["id"];
+		tile.id = id;
+	}
+	
+
+	tile.serializePixels = function(){
+		//console.log(JSON.stringify(this.pixelsArray).length*4+"bytes");
+		return JSON.stringify(this.pixelsArray);
 	}
 
-	function convertPixelsArrayToCanvas(pixelsArray, width, height){
-		
-		var mCanvas = newEl('canvas');
-		mCanvas.width = width;
-		mCanvas.height = height;
-		
-		var mContext = mCanvas.getContext('2d');
-		var mImgData = mContext.createImageData(width, height);
-		
-		var srcIndex=0, dstIndex=0, curPixelNum=0;
-		
-		for (curPixelNum=0; curPixelNum<width*height;  curPixelNum++)
-		{
-			mImgData.data[dstIndex] = pixelsArray[srcIndex][0];		// r
-			mImgData.data[dstIndex+1] = pixelsArray[srcIndex][1];	// g
-			mImgData.data[dstIndex+2] = pixelsArray[srcIndex][2];	// b
-			mImgData.data[dstIndex+3] = 255; // 255 = 0xFF - constant alpha, 100% opaque
-			srcIndex += 1;
-			dstIndex += 4;
-		}
-		mContext.putImageData(mImgData, 0, 0);
 
-		mCanvas.convertToBase64 = function(){
-			return (String)(this.toDataURL());
-		}
-		return mCanvas;
+	tile.setId = function(id){
+		tile.id = id;
 	}
 
-	function convertBase64toCanvas(myImage,width,height){
-		var start = new Date().getMilliseconds();
-		var mCanvas = newEl("canvas");
-		mCanvas.width = width;
-		mCanvas.height = height;
-		mCanvas.setAttribute('style', "width:"+10*width+"px; margin-left: 2px; margin-bottom:2px; height:"+10*height+"px;"); // make it large enough to be visible
-		var mContext = mCanvas.getContext('2d');
-      	mContext.drawImage(myImage, 0, 0);
-        var end = new Date().getMilliseconds();
-		//console.log((end-start)+"ms");
-		return mCanvas;
-	}
+	
+	return tile;
+	//var likelihood = config["likelihood"];
+}
 
-	function convertCanvasToPixelsArray(mCanvas){
-		var mContext = mCanvas.getContext('2d');
-		console.log(mCanvas.width);
-		var mImgData = mContext.getImageData(0,0,mCanvas.width,mCanvas.height);
-		
-		var srcIndex=0, dstIndex=0, curPixelNum=0;
-		var pixelsArray = new Array(mCanvas.width*mCanvas.height);
-		for (curPixelNum=0; curPixelNum<mCanvas.width*mCanvas.height;  curPixelNum++)
-		{
-			pixelsArray[dstIndex] = new Array(3);
-			pixelsArray[dstIndex][0] = mImgData.data[srcIndex];	// r
-			pixelsArray[dstIndex][1] = mImgData.data[srcIndex+1];	// g
-			pixelsArray[dstIndex][2] = mImgData.data[srcIndex+2];	// b
-			//mImgData.data[dstIndex+3] = 255; // 255 = 0xFF - constant alpha, 100% opaque
-			dstIndex += 1;
-			srcIndex += 4;
-		}
-		return pixelsArray;
-	}
+function extractTile(dbObj){
+		return initialize_tile({id:dbObj["id"],x:dbObj["x"], y:dbObj["y"], zoomLevel:dbObj["zoomLevel"],width:7,height:7, base64DataUrl: dbObj["base64DataUrl"] });
+}
 
-	function initialize_tile(config){
-		var x = config["x"];
-		var y = config["y"];
-		var width = config["width"];
-		var height = config["height"];
-		
-		var zoomLevel = config["zoomLevel"];
-		var pixelsArray;
-		var tile = new Object();
-		tile.width = width;
-		tile.height = height;
-		tile.posX = x;
-		tile.posY = y;
-		tile.zoomLevel = zoomLevel;
-		if (config["base64DataUrl"]){
-			tile.base64DataUrl = config["base64DataUrl"];
+
+function initialize_pixelsArray(config){ //if num_of_samples is 1 then there is no sampling
+	var width = config["width"];
+	var height = config["height"];
+	var defaultColor = config["defaultColor"];
+	var pixelsArray = new Array(width*height);
+	for (var i = 0; i < pixelsArray.length; i++){
+		if (!defaultColor){
+			pixelsArray[i] = []
 		}
 		else {
-			var defaultColor = config["defaultColor"];
-			pixelsArray = initialize_pixelsArray({width: width,height: height,defaultColor: defaultColor});
-			tile.base64DataUrl = convertPixelsArrayToCanvas(pixelsArray,tile.width,tile.height).convertToBase64();
+			pixelsArray[i] = defaultColor;//[Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256) ]//defaultColor;
+
+
 		}
-		if (config["id"]){
-			id = config["id"];
-			tile.id = id;
+	}
+	return pixelsArray;
+}
+
+function render(config,dispatcher){
+		var tile = config["tile"];	
+		var myImage = new Image();
+		myImage.setAttribute("src", tile.base64DataUrl);
+		console.log(tile);
+		myImage.onload = function() {
+			var mCanvas = convertBase64toCanvas(myImage,tile.width,tile.height);
+			var start = new Date().getMilliseconds();
+			mCanvas.setAttribute('style', "width:"+10*this.width+"px; margin-left: 2px; margin-bottom:2px; height:"+10*this.height+"px;"); // make it large enough to be visible
+			document.body.appendChild( mCanvas );
+			//var end = new Date().getMilliseconds();
+			dispatcher.check();
 		}
-		
-
-		tile.serializePixels = function(){
-			//console.log(JSON.stringify(this.pixelsArray).length*4+"bytes");
-			return JSON.stringify(this.pixelsArray);
-		}
-
-
-
-		tile.updatePixels = function(db){
-			//console.log(tile["id"]);
-			db.updateTilePixelDataWithId(this["id"], this.base64DataUrl);
-		}
-
-		tile.setId = function(id){
-			tile.id = id;
-		}
-
-		
-		return tile;
-		//var likelihood = config["likelihood"];
 	}
 
-	function extractTile(dbObj){
-			return initialize_tile({id:dbObj["id"],x:dbObj["x"], y:dbObj["y"], zoomLevel:dbObj["zoomLevel"],width:7,height:7, base64DataUrl: dbObj["base64DataUrl"] });
-	}
-
-
-	function initialize_pixelsArray(config){ //if num_of_samples is 1 then there is no sampling
-		var width = config["width"];
-		var height = config["height"];
-		var defaultColor = config["defaultColor"];
-		var pixelsArray = new Array(width*height);
-		for (var i = 0; i < pixelsArray.length; i++){
-			if (!defaultColor){
-				pixelsArray[i] = []
+function addSample(config,dispatcher){
+	var tile = config["tile"]
+	var sample_list = config["sample"]
+	var samples_available = config["num_samples"];
+	var which_sample = config["which_sample"];
+	convertBase64ToPixelsArray(tile,function(pixelsArray){
+		var counter = 0;
+		var position = 0;
+		var tile_pixels = tile.width*tile.height;
+		var sample_pixels = sample_list.length;
+		var sample_offset = which_sample - 1;
+		for (var i = 0; i<sample_pixels; i++){
+			if (position+sample_offset>tile_pixels){
+				//TO INVERSE
 			}
 			else {
-				pixelsArray[i] = defaultColor;//[Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256) ]//defaultColor;
-
-
+				pixelsArray[position+sample_offset] = sample_list[i];
+				position += samples_available;
+				counter++;
 			}
 		}
-		return pixelsArray;
-	}
-
-	function render(config,dispatcher){
-			var tile = config["tile"];	
-			var myImage = new Image();
-			myImage.setAttribute("src", tile.base64DataUrl);
-			console.log(tile);
-			myImage.onload = function() {
-				var mCanvas = convertBase64toCanvas(myImage,tile.width,tile.height);
-				var start = new Date().getMilliseconds();
-				mCanvas.setAttribute('style', "width:"+10*this.width+"px; margin-left: 2px; margin-bottom:2px; height:"+10*this.height+"px;"); // make it large enough to be visible
-				document.body.appendChild( mCanvas );
-				//var end = new Date().getMilliseconds();
-				dispatcher.check();
-			}
-		}
-
-	function addSample(config,dispatcher){
-		var tile = config["tile"]
-		var sample_list = config["sample"]
-		var samples_available = config["num_samples"];
-		var which_sample = config["which_sample"];
-		convertBase64ToPixelsArray(tile,function(pixelsArray){
-			var counter = 0;
-			var position = 0;
-			var tile_pixels = tile.width*tile.height;
-			var sample_pixels = sample_list.length;
-			var sample_offset = which_sample - 1;
-			for (var i = 0; i<sample_pixels; i++){
-				if (position+sample_offset>tile_pixels){
-					//TO INVERSE
-				}
-				else {
-					pixelsArray[position+sample_offset] = sample_list[i];
-					position += samples_available;
-					counter++;
-				}
-			}
-			tile.base64DataUrl = convertPixelsArrayToCanvas(pixelsArray,tile.width,tile.height).convertToBase64();
-			
-			dispatcher.check();
-
-		});
-	}
-	function tileInsert(config,dispatcher){
-		var tile = config["tile"];
-		var db = config["db"];
-		db.insertTileData(tile.posX, tile.posY, tile.zoomLevel,tile.base64DataUrl);
+		tile.base64DataUrl = convertPixelsArrayToCanvas(pixelsArray,tile.width,tile.height).convertToBase64();
+		
 		dispatcher.check();
-	}
 
-	function tileFetchById(config,dispatcher){
+	});
+}
+
+function convertBase64ToPixelsArray(tile,whatToDoNext){
+	var base64DataUrl = tile.base64DataUrl;
+	var mCanvas = newEl("canvas");
+	var ctx = mCanvas.getContext("2d");
+	var pixelsArray = [];
+
+	var img = newEl("img");
+	img.onload = getRGB;
+	img.src = base64DataUrl;
+
+	function getRGB(){
+		mCanvas.width = img.width;
+		mCanvas.height = img.height;
+		ctx.drawImage(img,0,0);
+		var data = ctx.getImageData(0,0,mCanvas.width, mCanvas.height).data;
+		for (var i=0; i<data.length; i+=4){
+			pixelsArray.push([data[i],data[i+1],data[i+2]]);
+		}
+		whatToDoNext(pixelsArray);
+	}
+}
+
+$(function() {
+
+	
+	/*function tileFetchById(config,dispatcher){
 		var tile = config["tile"];
 		var db = config["db"];
 		var id = config["id"];
@@ -194,33 +205,13 @@ $(function() {
 		var render = config["render"];
 		db.fetchTileWithId(id,extractTile,render,dispatcher);
 
-	}
-	function convertBase64ToPixelsArray(tile,whatToDoNext){
-		var base64DataUrl = tile.base64DataUrl;
-		var mCanvas = newEl("canvas");
-		var ctx = mCanvas.getContext("2d");
-		var pixelsArray = [];
+	}*/
 
-		var img = newEl("img");
-		img.onload = getRGB;
-		img.src = base64DataUrl;
-
-		function getRGB(){
-			mCanvas.width = img.width;
-			mCanvas.height = img.height;
-			ctx.drawImage(img,0,0);
-			var data = ctx.getImageData(0,0,mCanvas.width, mCanvas.height).data;
-			for (var i=0; i<data.length; i+=4){
-				pixelsArray.push([data[i],data[i+1],data[i+2]]);
-			}
-			whatToDoNext(pixelsArray);
-		}
-	}
 
 	$(document).ready(function() {
 		db = createDatabaseIfNotExists(':memory:',2*1024*1024);
-		db.dropTilesTableIfExists();
-		db.createTilesTableIfNotExists();
+		dropTilesTableIfExists(db);
+		createTilesTableIfNotExists(db);
 		
 		var msg;
 
@@ -252,17 +243,28 @@ $(function() {
 				dispatcher.exec();
 			}
 		}
-
-		dispatcher.addNew({func: addSample, config: {tile: tile,sample: sample, num_samples: 6, which_sample: 1}});
-		dispatcher.addNew({func: render,config: {tile: tile}});
 		dispatcher.addNew({func: tileInsert,config: {tile: tile, db: db}});
+		//dispatcher.addNew({func: render,config: {tile: tile}});
+		dispatcher.addNew({func: tileInsert,config: {tile: tile, db: db}});
+		dispatcher.addNew({func: addSample, config: {tile: tile,sample: sample, num_samples: 6, which_sample: 1}});
 		dispatcher.addNew({func: addSample, config: {tile: tile,sample: sample, num_samples: 6, which_sample: 2}});
-		dispatcher.addNew({func: render,config: {tile: tile}});
 		dispatcher.addNew({func: addSample, config: {tile: tile,sample: sample, num_samples: 6, which_sample: 3}});
-		dispatcher.addNew({func: render,config: {tile: tile}});
-		dispatcher.addNew({func: tileFetchById,config: {id: 1, db: db, render: render, extractTile: extractTile}});
 		dispatcher.addNew({func: addSample, config: {tile: tile,sample: sample, num_samples: 6, which_sample: 4}});
 		dispatcher.addNew({func: addSample, config: {tile: tile,sample: sample, num_samples: 6, which_sample: 5}});
+		dispatcher.addNew({func: addSample, config: {tile: tile,sample: sample, num_samples: 6, which_sample: 6}});
+		dispatcher.addNew({func: tilePixelDataUpdate, config:{tile: tile, db:db}});
+		dispatcher.addNew({func: tileFetchById,config: {id: 2, db: db}});
+		//dispatcher.addNew({func: addSample, config: {tile: tile,sample: sample, num_samples: 6, which_sample: 2}});
+		//dispatcher.addNew({func: render,config: {tile: tile}});
+		//dispatcher.addNew({func: tileInsert,config: {tile: tile, db: db}});
+		//dispatcher.addNew({func: addSample, config: {tile: tile,sample: sample, num_samples: 6, which_sample: 3}});
+		//dispatcher.addNew({func: render,config: {tile: tile}});
+		//dispatcher.addNew({func: tileInsert,config: {tile: tile, db: db}});
+		//dispatcher.addNew({func: tileFetchById,config: {id: 4, db: db}});
+		//dispatcher.addNew({func: addSample, config: {tile: tile,sample: sample, num_samples: 6, which_sample: 4}});
+		//dispatcher.addNew({func: render,config: {tile: tile}});
+		//dispatcher.addNew({func: tileInsert,config: {tile: tile, db: db}});
+		//dispatcher.addNew({func: addSample, config: {tile: tile,sample: sample, num_samples: 6, which_sample: 5}});
 		setInterval(function(){dispatcher.check(); },1000);
 		
 
