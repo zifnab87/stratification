7f97f6d0fe210e2abfc3bd1284c93d0d60557f3d
Michail Michailidis <mmichaelid@gmail.com>
2013-06-29 19:03:09 -0400

    pseudocode

---
+++ pseudocode.js
@@ -0,0 +1,65 @@
+Request Format:
+
+var request_unprocessed = { tiles: [{x:?, y: ?}, ..] }
+var request_processed =  { tiles: [{ x: ?, y: ?, samples:[?,?,?,?] }, … ]  }
+
+example:
+
+request {
+	tiles: [
+		{x:32,y:231, samples[1,4]},
+		{x:32,y:231, samples[1,2,3,4]} 
+	]
+} 
+
+var cache; //global
+
+function process (request_unprocessed){
+	var request_processed = {};
+	for (tile : request_unprocessed) {
+		if (!cache.contains(tile)){
+			//var server_tile = server.get(tile.x,tile.y,tile.samples);
+			request_processed.tiles.push({x: tile.x,y: tile.y, samples: tile.samples})
+		}
+		else {
+			var tile_id = hash(tile.x+""+tile.y);
+			var cached_tile = cache.get(tile_id);
+			var samples_of_cached_tile = cached_tile.samples;
+			var samples_to_be_requested_by_server = tile.samples - cached_tile.samples;
+			if (sample_to_be_requested_by_server!=empty){
+				request_processed.tiles.push({x: tile.x,y: tile.y, samples_to_be_requested_by_server})
+			}
+		}
+	}
+
+	return request_processed;
+}
+
+function process (request){
+	foreach tile in request:
+		if cache doesnt contain tile: 
+			put tile in new_request
+		else:
+			get tile from the cache
+			remove the sample ids from request that exist in cached tile
+			if samples list not:
+				put the changed tile in the new_request
+			else
+				render tile from cache
+	return new_request
+}
+
+function server_request(new_request){
+	send server the request and get response
+	count space needed in cache based on the number of tiles and samples of each tile
+	sort tiles in cache in ascending likelihood order and ascending samples num order 
+	and remove one by one till the needed space is free
+	push in the cache the servers response
+}
+
+
+//possibly the server should send the response tiles compressed in base64
+
+
+
+
