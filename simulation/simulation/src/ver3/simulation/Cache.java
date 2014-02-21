64c8308bd5115553069672435b59589a216872af
Michail Michailidis <mmichaelid@gmail.com>
2014-02-20 20:33:12 -0500

    some minor changes

--- simulation/simulation/src/ver3/simulation/Cache.java
+++ simulation/simulation/src/ver3/simulation/Cache.java
@@ -1,9 +1,65 @@
 package ver3.simulation;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.TreeSet;
 
 import ver3.simulation.CachedTile;
 
 public class Cache {
-	private TreeSet<CachedTile> queue = new TreeSet<CachedTile>(CachedTile.importanceComparator);
+	
+	
+	private  Map<Integer,CachedTile> tiles = new HashMap<Integer, CachedTile>();
+	private TreeSet<CachedTile> queue;
+	
+	
+	
+	private TreeSet<CachedTile> updateQueue(){
+		this.queue = new TreeSet<CachedTile>(CachedTile.importanceComparator);
+		Iterator<Integer> iter = tiles.keySet().iterator();
+		while(iter.hasNext()){
+			this.queue.add(tiles.get(iter.next()));
+		}
+		return this.queue;
+	}
+	
+	/*private CachedTile queueFind(int hashCode) {
+		boolean found = false;
+		Iterator<CachedTile> iter = this.queue.iterator();
+		while(iter.hasNext()){
+			CachedTile cTile = iter.next();
+			if (cTile.id == hashCode){
+				found = true;
+				return cTile;
+			}
+		}
+		return null;
+	}
+	
+
+	private boolean queueContains(CachedTile toBeCached) {
+		boolean found = false;
+		Iterator<CachedTile> iter = this.queue.iterator();
+		while(iter.hasNext()){
+			CachedTile cTile = iter.next();
+			if (cTile.id == toBeCached.id){
+				found = true;
+				break;
+			}
+		}
+		return found;
+	}
+	
+	private void queueRemove(CachedTile toBeCached){
+		Iterator<CachedTile> iter = this.queue.iterator();
+		while(iter.hasNext()){
+			CachedTile cTile = iter.next();
+			if (cTile.id == toBeCached.id){
+				iter.remove();
+			}
+		}
+	}*/
+	
+	
 }
--- simulation/simulation/src/ver3/simulation/CachedTile.java
+++ simulation/simulation/src/ver3/simulation/CachedTile.java
@@ -26,4 +26,32 @@ public class CachedTile extends Tile {
 	}
 	
 	
+	public static Comparator<CachedTile> importanceComparator = new Comparator<CachedTile>(){
+		@Override
+		public int compare(CachedTile t1, CachedTile t2) {
+			
+			//probability and distance
+		    if (t1.totalImportance < t2.totalImportance ){
+		    	return -1;
+		    }
+		    else if (t1.totalImportance > t2.totalImportance  ){
+		    	return 1;
+		    }
+		   
+		    else if (t1.totalImportance == t2.totalImportance &&
+		    		t1.point.id < t2.point.id)
+		    {
+		    	return -1;
+		    }
+		    else if (t1.totalImportance == t2.totalImportance &&
+		    		t1.point.id > t2.point.id)
+		    {
+		    	return 1;
+		    }
+		    else {
+		    	return 0;
+		    }
+		}
+	};
+	
 }
